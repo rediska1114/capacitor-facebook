631bcd883ac60eec49b6feb77b0424043c024738
Stepan Kotov <rediska1114@gmail.com>
2022-12-22 17:04:27 +0600

    Initial commit

---
+++ android/src/main/java/com/rediska1114/plugins/facebook/Facebook.java
@@ -0,0 +1,11 @@
+package com.rediska1114.plugins.facebook;
+
+import android.util.Log;
+
+public class Facebook {
+
+    public String echo(String value) {
+        Log.i("Echo", value);
+        return value;
+    }
+}
